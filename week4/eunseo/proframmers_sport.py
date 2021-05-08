e245504d190e4bc5fd24e9c814f94fc79ffb5b33
eunseo2 <20180972@sungshin.ac.kr>
2021-05-08 20:53:37 +0900

    21/5/8

---
+++ week4/eunseo/proframmers_sport.py
@@ -0,0 +1,19 @@
+def solution(n, lost, reserve):
+    
+    n_lost = list(set(lost)-set(reserve))
+    n_reserve = list(set(reserve)-set(lost))
+    
+    answer= n - len(n_lost)
+    
+    for i in  n_lost :
+            
+        if i-1 in n_reserve :
+            answer +=1
+            n_reserve.remove(i-1)
+        elif i+1 in n_reserve :
+            answer +=1 
+            n_reserve.remove(i+1)
+
+            
+
+    return answer
